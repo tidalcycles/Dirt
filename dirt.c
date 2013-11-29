1a8812ff0b4adf956d2c62c8f32ed061c32cc629
alex <alex@slab.org>
2013-11-29 00:12:10 +0000

    add a help option. not that helpful but tells you what port it's listening on

--- dirt.c
+++ dirt.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
-
 #include <unistd.h>
+#include <string.h>
 
 #include "jack.h"
 #include "audio.h"
 #include "server.h"
 
 int main (int argc, char **argv) {
-  printf("init audio\n");
+  if (argc > 1 && strncmp(argv[1], "-h", 2) == 0) {
+      printf("Dirt - a software sampler, which listens for\n"
+             "OpenSoundControl messages on port %s.\n"
+             "Mainly used with Tidal http://yaxu.org/tidal/\n"
+             "Released as free software under the terms of the\n"
+             "GNU Public License version 3.0 and later.\n",
+             OSC_PORT
+             );
+      return(0);
+  }
+
+  fprintf(stderr, "init audio\n");
   audio_init();
-  printf("init osc\n");
+
+  fprintf(stderr, "init open sound control\n");
   server_init();
+
   sleep(-1);
   return(0);
 }
