55c0826fe458c68750daaa845aaad5e694a8678d
Damián Silvani <munshkr@gmail.com>
2015-03-21 15:51:53 -0300

    Define `thpool` module for managing a thread pool
    This is useful for parallelizing certain independent tasks, like file
    reading or effects preprocessing.

---
+++ thpool.h
@@ -0,0 +1,19 @@
+#ifndef __THPOOL_H__
+#define __THPOOL_H__
+
+#include <stdbool.h>
+
+#include "jobqueue.h"
+
+typedef struct thpool thpool_t;
+
+// Initialize a thread pool of a specified size
+thpool_t* thpool_init(unsigned int num_threads);
+
+// Push a job to the queue
+bool thpool_add_job(thpool_t* p, void *(*function)(void*), void* args);
+
+// De-allocated thread pool and its associated structures
+void thpool_destroy(thpool_t* p);
+
+#endif
